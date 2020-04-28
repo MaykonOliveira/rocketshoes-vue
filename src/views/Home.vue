57d76be23f36164916aab9d8639624a969e4bbc8
Maykon A. Oliveira <moliveira.anschau@gmail.com>
2020-04-27 22:15:20 -0300

    Finalized initial layout of home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,98 @@
 <template>
-  <div class="home"></div>
+  <ul id="grid-list">
+    <li class="grid-item">
+      <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" />
+      <strong>Tênis de Caminhada Leve Confortável</strong>
+      <span>R$ 179,90</span>
+
+      <button>
+        <div>
+          <i class="fas fa-cart-plus"></i>
+          0
+        </div>
+        <span>ADICIONAR AO CARRINHO</span>
+      </button>
+    </li>
+  </ul>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      products: [],
+    };
+  },
 };
 </script>
+
+<style scoped lang="scss">
+#grid-list {
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(3, 1fr);
+  list-style: none;
+}
+
+.grid-item {
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 20px;
+
+  img {
+    align-self: center;
+    max-width: 250px;
+  }
+
+  > strong {
+    font-size: 16px;
+    line-height: 20px;
+    color: #333;
+    margin-top: 5px;
+  }
+
+  > span {
+    font-size: 21px;
+    font-weight: bold;
+    margin: 5px 0 20px;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: #7159c1;
+    color: #fff;
+    border: 0;
+    border-radius: 4px;
+    overflow: hidden;
+    margin-top: auto;
+    transition: background 0.2s;
+
+    &:hover {
+      background: #573fa8;
+    }
+
+    div {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 12px;
+      background: rgba(0, 0, 0, 0.1);
+
+      svg {
+        margin-right: 5px;
+      }
+    }
+
+    span {
+      flex: 1;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+}
+</style>
